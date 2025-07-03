0096204e7b97ea6b39439c65e016846e3206c36e
Akson Sam Varghese <aksonsam@gmail.com>
2025-07-03 19:15:57 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,19 +1,20 @@
 import requests
+import pytest
 
-# Define the API endpoint
-url = "http://0.0.0.0:8005/generate"
+def test_generate()
+    # Define the API endpoint
+    url = "http://0.0.0.0:8005/generate"
+    
+    # Define the payload
+    payload = {
+        "text": "Once upon a time"
+    }
+    
+    # Make a POST request to the API
+    response = requests.post(url, json=payload)
+    assert response.status_code == 200, response.content
 
-# Define the payload
-payload = {
-    "text": "Once upon a time"
-}
-
-# Make a POST request to the API
-response = requests.post(url, json=payload)
-
-# Check the response status and print the generated text
-if response.status_code == 200:
-    print("Generated Text:", response.json()["generated_text"])
-else:
-    print("Failed to generate text. Status code:", response.status_code)
-    print("Response:", response.text)
+    response_json = response.json()
+    # Check the response status and print the generated text
+    assert "generated_text" in response_json
+    assert isinstance(response_json["generated_text"])
